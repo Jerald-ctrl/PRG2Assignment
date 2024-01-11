b0afc32cd40d77514c3370c5d65910c1870298f0
Jerald-Tee <S10259842@connect.np.edu.sg>
2024-01-11 16:07:05 +0800

    Initial Commit

---
+++ S10259842_PRG2Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace S10259842_PRG2Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
