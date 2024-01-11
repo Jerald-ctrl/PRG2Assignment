58f3824c54dd79c2202c6c641ded887d69937258
Jerald-ctrl <150308558+Jerald-Tee@users.noreply.github.com>
2024-01-11 16:29:21 +0800

    Update Program.cs

--- S10259842_PRG2Assignment/Program.cs
+++ S10259842_PRG2Assignment/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace S10259842_PRG2Assignment
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I have edited this!");
         }
     }
 }
