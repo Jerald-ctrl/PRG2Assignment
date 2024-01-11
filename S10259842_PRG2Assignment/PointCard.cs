f3197bf5239e68c6bd38869a432f72c1a9405451
Jerald-ctrl <150308558+Jerald-Tee@users.noreply.github.com>
2024-01-11 17:09:08 +0800

    Create PointCard.cs

---
+++ S10259842_PRG2Assignment/PointCard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10259842_PRG2Assignment
+{
+    internal class PointCard
+    {
+        private int points;
+        private int punchCard;
+        private string tier;
+
+
+        public int Points
+        { get { return points; } set { points = value; } }
+
+        public int PunchCard
+        { get { return punchCard; } set { punchCard = value; } }
+
+        public string Tier
+        { get { return tier; } set { tier = value; } }
+
+        public PointCard()
+        {
+
+        }
+
+        public PointCard(int points, int punchCard)
+        {
+            Points = points;
+            PunchCard = punchCard;
+        }
+
+        public void AddPoints(int pointAdd)
+        {
+            Points += pointAdd;
+        }
+
+        public void RedeemPoints(int pointDed)
+        {
+            if (Points >= pointDed)
+            {
+                Points -= pointDed;
+            }
+
+        }
+
+        public void Punch()
+        {
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"Points: {Points}. PunchCard: {PunchCard}. Tier: {Tier}.";
+        }
+
+
+    }
+}
