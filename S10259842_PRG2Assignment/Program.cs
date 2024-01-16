24bf404e61c2b87ea072432e9e7af890423eea95
keagan alexander <s10259305@connect.np.edu.sg>
2024-01-16 14:44:13 +0800

    created DisplayCustomers() method (basic feature 1)

--- S10259842_PRG2Assignment/Program.cs
+++ S10259842_PRG2Assignment/Program.cs
@@ -14,16 +14,19 @@ namespace S10259842_PRG2Assignment
             {
                 using (StreamReader cReader = new StreamReader("customers.csv"))
                 {
-                    string[] headers = Console.ReadLine().Split(",");
+                    string[] headers = cReader.ReadLine().Split(",");
+                    Console.WriteLine($"{headers[0],-12} {headers[1],-12} {headers[2],-12} {headers[3],-20} {headers[4],-20} {headers[5],-12}");
 
-                    string? line;
+                    string ? line;
                     while ((line = cReader.ReadLine()) != null)
                     {
                         string[] cInfo = line.Split(",");
-
+                        Console.WriteLine($"{cInfo[0], -12} {cInfo[1],-12} {cInfo[2],-12} {cInfo[3],-20} {cInfo[4],-20} {cInfo[5],-12}");
                     }
                 }
             }
+
+            DisplayCustomers();
         }
     }
 }
