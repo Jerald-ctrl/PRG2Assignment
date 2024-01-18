b3cc2e538d5f95555cdd7482dc9bbf52a10426f5
Jerald-ctrl <150308558+Jerald-Tee@users.noreply.github.com>
2024-01-18 16:13:50 +0800

    Customer and Order.cs
    Currently needs to test if its compatible with the other files

---
+++ S10259842_PRG2Assignment/Customer.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10259842_PRG2Assignment
+{
+    internal class Customer
+    {
+        private string name;
+        private int memberId;
+        private DateTime dob;
+        private Order currentOrder;
+        private List<Order> orderHistory = new List<Order>();
+        private PointCard rewards;
+
+        public string Name
+        { get { return name; } set { name = value; } }
+
+        public int MemberId
+        { get { return memberId; } set {  memberId = value; } }
+
+        public DateTime Dob
+        { get { return dob; } set {  dob = value; } }
+
+
+        public Order CurrentOrder
+        { get { return currentOrder; } set { currentOrder = value; } }
+
+
+    }
+}
---
+++ S10259842_PRG2Assignment/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10259842_PRG2Assignment
+{
+    class Order
+    {
+        private int id;
+        private DateTime timeReceived;
+        private DateTime? timeFulfilled;
+        private List<IceCream> iceCreamList = new List<IceCream>();
+
+
+        public int Id
+        { get { return id; } set { id = value; } }
+
+        public DateTime TimeReceived
+        { get { return timeReceived; } set {  timeReceived = value; } } 
+
+        public DateTime? TimeFulfilled
+        { get { return timeFulfilled; } set { timeFulfilled = value; } } 
+
+
+    }
+}
